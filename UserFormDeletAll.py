c01506f99fe23966f958f8614d8c619c487e06b9
Andrey Prichodko <5ray69@ukr.net>
2022-12-28 21:50:22 +0200

    1 commit

---
+++ UserFormDeletAll.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import clr
+clr.AddReference('System.Drawing')
+clr.AddReference('System.Windows.Forms')
+
+import System
+import System.Drawing
+import System.Windows.Forms
